06880d9ad40b6181b9bd706eeafb0b73e3b87bb9
vuqu-git <vuqu@fantasymail.de>
2025-01-28 15:50:09 +0100

    Alter Hello string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jonas and Eduard!");
+        System.out.println("Hello, Jonas!");
     }
 }
