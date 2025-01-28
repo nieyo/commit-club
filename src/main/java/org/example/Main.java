1cbb706eb145d87a4a7b2038de90b4203d670ff4
Eddy-Henne <162419817+Eddy-Henne@users.noreply.github.com>
2025-01-28 16:04:05 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Jonas :)");
         System.out.println("Hello, Team!");
+        //Eddy war auch hier
     }
 }
